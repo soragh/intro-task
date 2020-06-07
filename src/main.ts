888e26f32ad8939e739b2430c97c59cbab7f9c0b
Soragh <srghrml@gmail.com>
2020-06-07 18:33:16 +0545

    'introcept'

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import {join} from 'path'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  
+  const port = process.env.PORT || 3000
   app.useStaticAssets(join(__dirname,'/node_modules/bootstrap/dist/css'))
   app.useStaticAssets(join(__dirname,'..','public'))
   app.setBaseViewsDir(join(__dirname,'..','views'))
   app.setViewEngine('hbs',)
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
