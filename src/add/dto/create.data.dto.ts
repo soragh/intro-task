2ce2f9410f678d533a04c9a9bdbe14046d96b6ce
Soragh <srghrml@gmail.com>
2020-06-09 17:17:46 +0545

    'introcept'

---
+++ src/add/dto/create.data.dto.ts
@@ -0,0 +1,12 @@
+export class CreateDataDto{
+    readonly name: string
+    readonly gender: string
+    readonly phone: string
+    readonly email: string
+    readonly address: string
+    readonly nationality: string
+    readonly dob: string
+    readonly education: string
+    readonly mode_of_contact: string
+    readonly alias: string
+}
